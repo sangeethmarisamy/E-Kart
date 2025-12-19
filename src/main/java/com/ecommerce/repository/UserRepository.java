879cee55fa6f58d8f0d9236cf617808be92cb421
sangeethmarisamy <kit28.24bcb046@gmail.com>
2025-12-19 16:23:40 +0530

    upto Login Page

---
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.ecommerce.repository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ecommerce.entity.User;
+public class UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByEmail(String email);
+    public boolean existsByUsername(String username);
+    public boolean existsByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.username = ?1 OR u.email = ?2")
+    public Optional<User> findByUsernameOrEmail(String username, String email);
+}
